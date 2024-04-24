afe13aba0892a968d8dfcb1d7e668a1a4c589083
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-04-24 19:18:44 -0300

    Consertando erro de gols contra e pro
    Consertando erro de gols contra e pro e adicionando função que ordena por pontos.
    
    Co-Authored-By: João Eduardo <125677846+joao-eduardo17@users.noreply.github.com>

--- scripts/index.js
+++ scripts/index.js
@@ -44,16 +44,41 @@ function renderizaClubes(){
 function adicionaClube(nome, pts = 0, pj = 0, vit = 0, emp = 0, der = 0, gp = 0, gc = 0, sg = 0){
     const clube = {
         "Nome": nome,
-        "Pontos": pts,
-        "Partidas": pj,
-        "Vitorias": vit,
-        "Empates": emp,
-        "Derrotas": der,
-        "Gols-Pro": gp,
-        "Gols-Contra": gc,
-        "Saldo-Gols": sg
+        "Pontos": parseInt(pts),
+        "Partidas": parseInt(pj),
+        "Vitorias": parseInt(vit),
+        "Empates": parseInt(emp),
+        "Derrotas": parseInt(der),
+        "Gols-Pro": parseInt(gp),
+        "Gols-Contra": parseInt(gc),
+        "Saldo-Gols": parseInt(sg)
     }
     clubes.push(clube)
+}
+
+function casoEmpate(clube, gp, gc) {
+    clube.Partidas = parseInt(clube.Partidas) + 1
+    clube.Pontos = parseInt(clube.Pontos) + 1
+    clube.Empates = parseInt(clube.Empates) + 1
+    clube['Gols-Pro'] = parseInt(clube['Gols-Pro']) + parseInt(gp)
+    clube['Gols-Contra'] = parseInt(clube['Gols-Contra']) + parseInt(gc)
+}
+
+function casoVitoria(clube, gp, gc) {
+    clube.Partidas = parseInt(clube.Partidas) + 1
+    clube.Pontos = parseInt(clube.Pontos) + 3
+    clube.Vitorias = parseInt(clube.Vitorias) + 1
+    clube['Gols-Pro'] = parseInt(clube['Gols-Pro']) + parseInt(gp)
+    clube['Gols-Contra'] = parseInt(clube['Gols-Contra']) + parseInt(gc)
+    clube['Saldo-Gols'] = parseInt(clube['Saldo-Gols']) + gp - gc 
+}
+
+function casoDerrota(clube, gp, gc) {
+    clube.Partidas = parseInt(clube.Partidas) + 1
+    clube.Derrotas = parseInt(clube.Derrotas) + 1
+    clube['Gols-Pro'] = parseInt(clube['Gols-Pro']) + parseInt(gp)
+    clube['Gols-Contra'] = parseInt(clube['Gols-Contra']) + parseInt(gc)
+    clube['Saldo-Gols'] = parseInt(clube['Saldo-Gols']) + gp - gc
 }
 
 function adicionaPartida(timeCasa, timeFora, placar){
@@ -62,11 +87,44 @@ function adicionaPartida(timeCasa, timeFora, placar){
         "Fora": timeFora,
         "Placar": placar
     }
+
     clubes.forEach((clube) => {
         if(clube.Nome === timeCasa) {
-            //
+            placar = partida.Placar
+            gp = placar.substring(0, placar.indexOf('X'))
+            gc = placar.substring(placar.indexOf('X')+1)
+            if(gp == gc) {
+                casoEmpate(clube, gp, gc)
+            } else if(parseInt(gp) > parseInt(gc)) {
+                casoVitoria(clube, gp, gc)
+            } else {
+                casoDerrota(clube, gp, gc)
+            }
+        }
+        
+        if(clube.Nome === timeFora) {
+            placar = partida.Placar
+            gp = placar.substring(0, placar.indexOf('X'))
+            gc = placar.substring(placar.indexOf('X')+1)
+            if(gp == gc) {
+                casoEmpate(clube, gc, gp)
+            } else if(parseInt(gp) > parseInt(gc)) {
+                casoDerrota(clube, gc, gp)
+            } else {
+                casoVitoria(clube, gc, gp)
+            }
         }
     })
+}
+
+function ordenaPontos(a, b){
+    if(a.Pontos > b.Pontos){
+        return -1 
+    } else if(a.Pontos < b.Pontos){
+        return 1
+    } else {
+        return 0
+    }
 }
 
 adicionaClube("Corinthians")
@@ -89,6 +147,11 @@ adicionaClube("Fortaleza")
 adicionaClube("Athlético Paranaense")
 adicionaClube("Cuiabá")
 adicionaClube("Bragantino")
+adicionaPartida("Corinthians", "Palmeiras", "10X0")
+adicionaPartida("Cuiabá", "Vitória", "1X1")
+adicionaPartida("Athlético Paranaense", "Fortaleza", "0X2")
+clubes.sort(ordenaPontos)
+
 renderizaClubes()
 
-//clubes.toSorted((a, b) => a.Nome.localeCompare(b.Nome))
+//clubes.sort((a, b) => a.Nome.localeCompare(b.Nome))
