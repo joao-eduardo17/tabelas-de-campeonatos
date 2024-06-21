828387649da6a7925a89c6803209dfe911010c0e
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-06-20 22:34:55 -0300

    feat(frontend): Criando página Campeonato
    Criando página Campeonato.

---
+++ frontend/src/pages/campeonato/index.tsx
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import Tabela, { Clube } from "../../components/tabela";
+import CadastraPartida from "../../components/modais/cadastraPartida";
+import CadastraTime from "../../components/modais/cadastraTime";
+
+export default class Campeonato extends Component {
+    render() {
+        const titulos = ["POS", "CLUBE", "PTS", "JOGOS", "VIT", "EMP", "DER", "SG", "GP", "GC"]
+        const clubes: Clube[] = [
+            {
+            nome: "Corinthians",
+            pontos: 39,
+            jogos: 15,
+            vitorias: 12,
+            empates: 3,
+            derrotas: 0,
+            sg: 40,
+            gp: 45,
+            gc: 5
+            },
+            {
+            nome: "Palmeiras",
+            pontos: 3,
+            jogos: 15,
+            vitorias: 0,
+            empates: 3,
+            derrotas: 12,
+            sg: -35,
+            gp: 5,
+            gc: 40
+            }
+        ]
+        
+        return (
+            <>
+                <Tabela titulos={titulos} clubes={clubes}/>
+                <CadastraPartida/>
+                <CadastraTime/>
+            </>
+        )
+    }
+}
---
+++ frontend/src/pages/criacaoCampeonato/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+//Tela para adicionar times antes de confirmar o campeonato
+export default class CriacaoCampeonato extends Component {
+    render() {
+        return (
+            <></>
+        )
+    }
+}
