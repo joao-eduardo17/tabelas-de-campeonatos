82f60799d743a716767c9e2a017240a091ec3956
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2024-06-20 21:54:03 -0300

    feat(backend): Verificador de partidas
    Verificador de partidas repetidas e se o time já jogou aquela partida

--- backend/src/main/java/com/campeonatos/controllers/Verificador.java
+++ backend/src/main/java/com/campeonatos/controllers/Verificador.java
@@ -4,11 +4,37 @@ import java.util.ArrayList;
 
 import com.campeonatos.models.Partida;
 
+import main.java.com.campeonatos.models.Rodada;
+
 public class Verificador {
-    priavte a = 0;
+    private ArrayList<Rodada> rodadas;
+
+    public Verificador(ArrayList<Rodada> rodadas) {
+        this.rodadas = rodadas;
+    }
 
     public boolean verificaPartidaRepetida(Partida partida) {
-        ArrayList p = new ArrayList();
-        
+        Boolean repetiu = false;
+        this.rodadas.forEach((rodada) -> {
+            rodada.getRodada().forEach((partida1) -> {
+                if(partida1.getCasa().equals(partida.getCasa()) && partida1.getFora().equals(partida.getFora())) {
+                    repetiu.set(true);
+                }
+            });
+        });
+        return repetiu.getBoolean();
     }
+
+    public boolean verificaSeJogou(String nome) {
+        Boolean jogou = false;
+        this.rodadas.forEach((rodada) -> {
+            rodada.getRodada().forEach((partida) -> {
+                if(partida.getCasa().equals(nome) || partida.getFora().equals(nome)) {
+                    jogou.set(true);
+                }
+            });
+        });
+        return jogou.getBoolean();
+    }
+
 }
